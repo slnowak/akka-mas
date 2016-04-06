00db95a9e44d23be0360f0fd0f28dc673e56abda
novy <slnowak154@gmail.com>
2016-04-06 15:58:38 +0200

    Project setup

---
+++ build.sbt
@@ -0,0 +1,15 @@
+name := "akka-mas"
+version := "1.0"
+scalaVersion := "2.11.8"
+
+val akkaVersion = "2.4.3"
+
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor" % akkaVersion,
+  "com.typesafe.akka" %% "akka-remote" % akkaVersion,
+  "com.typesafe.akka" %% "akka-cluster" % akkaVersion,
+  "com.typesafe.akka" %% "akka-cluster-metrics" % akkaVersion,
+  "com.typesafe.akka" %% "akka-cluster-tools" % akkaVersion,
+  "com.typesafe.akka" %% "akka-multi-node-testkit" % akkaVersion,
+  "org.scalatest" %% "scalatest" % "2.2.6" % "test"
+)
---
+++ src/main/scala/pl/edu/agh/akka/mas/App.scala
@@ -0,0 +1,29 @@
+package pl.edu.agh.akka.mas
+
+import akka.actor.{ActorSystem, Props}
+import com.typesafe.config.ConfigFactory
+
+/**
+  * Created by novy on 06.04.16.
+  */
+object App {
+  def main(args: Array[String]): Unit = {
+    if (args.isEmpty)
+      startup(Seq("2551", "2552", "0"))
+    else
+      startup(args)
+  }
+
+  def startup(ports: Seq[String]): Unit = {
+    ports foreach { port =>
+      // Override the configuration of the port
+      val config = ConfigFactory.parseString("akka.remote.netty.tcp.port=" + port).
+        withFallback(ConfigFactory.load())
+
+      // Create an Akka system
+      val system = ActorSystem("ClusterSystem", config)
+      // Create an actor that handles cluster domain events
+      system.actorOf(Props[SimpleClusterListener], name = "clusterListener")
+    }
+  }
+}
